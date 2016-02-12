549de898dfc973380d7dfa4973fef2e9f390f885
val <val@kreit.ru>
2016-02-12 16:58:44 +0500

    gitHub: future#1 : add settings "Skip if unFocuse"
    tunung

--- in0k_lazIdeSRC_SourceEditor_onActivate.pas
+++ in0k_lazIdeSRC_SourceEditor_onActivate.pas
@@ -1,38 +1,46 @@
 unit in0k_lazIdeSRC_SourceEditor_onActivate;
 
-{$mode objfpc}{$H+}
-
-// уходим от подхода DEBUG .. используем для "прямой отладки"
-// http://wiki.freepascal.org/Extending_the_IDE#Debugging_the_IDE
-
-{todo: в режиме DEBUG: _WSE_set_ ? разобраться в выскакивающей подсказкой, генерит сообщение, это плохо наверно}
+{todo: описание и документация}
 
 interface
 
-{$ifDef In0k_lazIdeSRC_SourceEditor_onActivate_DebugLOG_mode}
-    {$define _debugLOG_}
-{$endIf}
-{.$define _debugLOG_}
+{%region --- Описание НАСТРОЕК уровня КОМПИЛЯЦИИ ----------------- /fold }
 
-uses {$ifDef _debugLOG_}SysUtils,Dialogs,{$endIf}
+//// ВНИМАНИЕ !!!
+//// настройки могут быть ПЕРЕОПРЕДЕЛЕНЫ ниже при подключении
+//// файла настроек "компанента-Расширения" (`in0k_lazExt_SETTINGs.inc`).
+
+//--- # DebugLOG_mode ----------------------------------------------------------
+// Режим логирования.
+//  В код включаются вызовы `DEBUG` с описанием текущих событий и состояний.
+//
+//{$define in0k_lazIdeSRC_SourceEditor_onActivate__DebugLOG_mode}
+//
+//------------------------------------------------------------------------------
+
+//--- # inFocusONLY ------------------------------------------------------------
+// Только в АКТИВНОМ окне.
+//  Пользовательское событие `onEvent` вызывается ТОЛЬКО если редактор кода
+//  находится в фокусе (тоесть окно "Редактор Кода" АКТИВНО).
+//
+//{$define in0k_lazIdeSRC_SourceEditor_onActivate__inFocusONLY}
+//
+//------------------------------------------------------------------------------
+
+{%endregion}
+
+{$i in0k_lazExt_SETTINGs.inc} //< настройки "компанента-Расширения".
+//< Можно смело убирать, так как будеть работать только в моей специальной
+//< "системе имен и папок" `in0k_LazExt_..`.
+
+
+uses {$ifDef in0k_lazIdeSRC_SourceEditor_onActivate__DebugLOG_mode}in0k_lazExt_DEBUG,{$endIf}
     SrcEditorIntf, //< you must use IDEIntf
-    Forms, Classes;//,
+    Forms, Classes;
 
 type
 
  tIn0k_lazIdeSRC_SourceEditor_onActivate=class
-  {%region --- debug Event LOG ------------------------------------ /fold}
-  {$ifDef _debugLOG_}
-  protected
-   _onDbgLOG_:TGetStrProc;
-    procedure _do_onDbgLOG_(const text:string); inline;
-    procedure  DEBUG       (const text:string); inline;
-    procedure  DEBUG(const mTYPE,mTEXT:string); inline;
-    function   addr2txt    (const value:pointer):string; inline;
-  public
-    property onDebug:TGetStrProc read _onDbgLOG_ write _onDbgLOG_;
-  {$endIf}
-  {%endRegion}
   {%region --- onActivate EVENT ----------------------------------- /fold}
   protected
   _onEvent_:TNotifyEvent;
@@ -43,6 +51,7 @@ type
    _ide_object_ESE_:TSourceEditorInterface;
     procedure _ESE_set_(const value:TSourceEditorInterface);
   {%endRegion}
+  {$ifDef in0k_lazIdeSRC_SourceEditor_onActivate__inFocusONLY}
   {%region --- Active Window SourceEditor ------------------------- /fold}
   private //< текущee АКТИВНОЕ окно редактирования
    _ide_object_WSE_:TSourceEditorWindowInterface;
@@ -53,76 +62,49 @@ type
     procedure _WSE_rePlace_onDeactivate_(const wnd:tForm);
     procedure _WSE_reStore_onDeactivate_(const wnd:tForm);
   {%endRegion}
+  {$endIf}
   {%region --- IdeEVENT ------------------------------------------- /fold}
-  private //< обработка событий IDE Lazarus
+  private //< основное событие
     procedure _ideEvent_SUMMARY;
+  private //< обработка событий IDE Lazarus
     procedure _ideEvent_semEditorActivate({%H-}Sender:TObject);
+    {$ifDef in0k_lazIdeSRC_SourceEditor_onActivate__inFocusONLY}
     procedure _ideEvent_semWindowFocused (     Sender:TObject);
+    {$endIf}
   {%endRegion}
   public
     property    onEvent:TNotifyEvent read _onEvent_ write _onEvent_;
-    property    SourceEditorWindow:TSourceEditorWindowInterface read _ide_object_WSE_;
+    //property    SourceEditorWindow:TSourceEditorWindowInterface read _ide_object_WSE_;
     property    SourceEditor      :TSourceEditorInterface read _ide_object_ESE_;
   public
     constructor Create;
-    procedure   LazarusIDE_SetUP;
-    procedure   LazarusIDE_Clean;
+    procedure   LazarusIDE_SetUP; virtual;
+    procedure   LazarusIDE_Clean; virtual;
   end;
 
 implementation
+
+{%region --- возня с ДЕБАГОМ -------------------------------------- /fold}
+{$if defined(in0k_lazIdeSRC_FuckUpForm__DebugLOG_mode) AND declared(in0k_lazIde_DEBUG)}
+    // `in0k_lazIde_DEBUG` - это функция ИНДИКАТОР что используется
+    //                       моя "система имен и папок"
+    {$define _debugLOG_}     //< типа да ... можно делать ДЕБАГ отметки
+{$else}
+    {$undef _debugLOG_}
+{$endIf}
+{%endregion}
 
 constructor tIn0k_lazIdeSRC_SourceEditor_onActivate.Create;
 begin
    _onEvent_:=nil;
    _ide_object_ESE_:=nil;
+    {$ifDef in0k_lazIdeSRC_SourceEditor_onActivate__inFocusONLY}
    _ide_object_WSE_:=nil;
    _ide_object_WSE_onDeactivate_original_:=nil;
-end;
+    {$endIf}
+ end;
 
 //------------------------------------------------------------------------------
-
-{%region --- debug Event LOG -------------------------------------- /fold}
-// для работы необходимо
-//  # `DEFINE`, определить глобальный или локальный
-//      - глобальный: `In0k_lazIdeSRC_SourceEditor_onActivate_DebugLOG_mode`
-//      - локальный : `_debugLOG_`
-//  # указать обработчик события `onDebug`
-{$ifDef _debugLOG_}
-
-const
-  _c_DBG_PleaseReport_=
-        LineEnding+
-        'EN: Please report this error to the developer.'+LineEnding+
-        'RU: Пожалуйста, сообщите об этой ошибке разработчику.'+
-        LineEnding;
-
-const
-  _c_DBG_addr_='$';
-  _c_DBG_f_O_ ='[';
-  _c_DBG_f_C_ =']';
-
-procedure tIn0k_lazIdeSRC_SourceEditor_onActivate._do_onDbgLOG_(const text:string);
-begin
-    if Assigned(_onDbgLOG_) then _onDbgLOG_(text);
-end;
-
-procedure tIn0k_lazIdeSRC_SourceEditor_onActivate.DEBUG(const text:string);
-begin
-   _do_onDbgLOG_(text);
-end;
-
-procedure tIn0k_lazIdeSRC_SourceEditor_onActivate.DEBUG(const mTYPE,mTEXT:string);
-begin
-    DEBUG(_c_DBG_f_O_+mTYPE+_c_DBG_f_C_+' '+mTEXT);
-end;
-
-function  tIn0k_lazIdeSRC_SourceEditor_onActivate.addr2txt(const value:pointer):string;
-begin
-    result:=_c_DBG_addr_+IntToHex({%H-}PtrUint(value),sizeOf(PtrUint)*2);
-end;
-
-{$endIf}
-{%endRegion}
 
 {%region --- Active Editor SourceEditor --------------------------- /fold}
 
@@ -136,6 +118,101 @@ end;
 
 {%endRegion}
 
+{%region --- IdeEVENT semEditorActivate --------------------------- /fold}
+
+// основное рабочее событие
+procedure tIn0k_lazIdeSRC_SourceEditor_onActivate._ideEvent_SUMMARY;
+var tmpSourceEditor:TSourceEditorInterface;
+begin
+    {*1> причины использования _ide_object_ESE_
+        механизм приходится использовать из-за того, что
+        при переключение "Вкладок Редактора Исходного Кода" вызов данного
+        события происходит аж 3(три) раза. Используем только ПЕРВЫЙ вход.
+        -----
+        еще это событие происходит КОГДА идет навигация (прыжки внутри файла)
+    }
+    if Assigned(SourceEditorManagerIntf) then begin //< запредельной толщины презерватив
+        tmpSourceEditor:=SourceEditorManagerIntf.ActiveEditor;
+        if Assigned(tmpSourceEditor) then begin //< чуть потоньше, но тоже толстоват
+            if (tmpSourceEditor<>_ide_object_ESE_) then begin
+               _ESE_set_(tmpSourceEditor);
+            end
+            else begin
+                {$ifDEF _debugLOG_}
+                DEBUG('SKIP','already processed');
+                {$endIf}
+            end;
+        end
+        else begin
+           _ESE_set_(nil);
+            {$ifDEF _debugLOG_}
+            DEBUG('ER','ActiveEditor is NULL');
+            {$endIf}
+        end;
+    end
+    else begin
+        {$ifDEF _debugLOG_}
+        DEBUG('ER','IDE not ready');
+        {$endIf}
+    end;
+end;
+
+//------------------------------------------------------------------------------
+
+procedure tIn0k_lazIdeSRC_SourceEditor_onActivate._ideEvent_semEditorActivate(Sender:TObject);
+begin
+    {$ifDEF _debugLOG_}
+    DEBUG('ideEVENT:semEditorActivate','--->>>'+' sender'+addr2txt(Sender));
+    {$endIf}
+
+    {$ifDef in0k_lazIdeSRC_SourceEditor_onActivate__inFocusONLY}
+    // запускаемся только если окно редактирования в ФОКУСЕ
+    if Assigned(_ide_object_WSE_) then _ideEvent_SUMMARY
+    else begin
+        {$ifDEF _debugLOG_}
+        DEBUG('SKIP','ActiveSourceWindow is UNfocused');
+        {$endIf}
+    end;
+    {$else}
+   _ideEvent_SUMMARY;
+    {$endIf}
+
+    {$ifDEF _debugLOG_}
+    DEBUG('ideEVENT:semEditorActivate','---<<<');
+    {$endIf}
+end;
+
+{$ifDef in0k_lazIdeSRC_SourceEditor_onActivate__inFocusONLY}
+procedure tIn0k_lazIdeSRC_SourceEditor_onActivate._ideEvent_semWindowFocused(Sender:TObject);
+begin
+    {$ifDEF _debugLOG_}
+    DEBUG('ideEVENT:semWindowFocused','--->>>'+' sender'+addr2txt(Sender));
+    {$endIf}
+
+    if Assigned(Sender) and (Sender is TSourceEditorWindowInterface) then begin
+       _WSE_set_(TSourceEditorWindowInterface(Sender));
+        if Assigned(_ide_object_WSE_) then _ideEvent_SUMMARY
+        else begin
+            {$ifDEF _debugLOG_}
+            DEBUG('SKIP WITH ERROR','BIG ERROR: ower _ide_Window_SEW_ found');
+            {$endIf}
+        end;
+    end
+    else begin
+        {$ifDEF _debugLOG_}
+        DEBUG('SKIP','Sender undef');
+        {$endIf}
+    end;
+
+    {$ifDEF _debugLOG_}
+    DEBUG('ideEVENT:semWindowFocused','---<<<');
+    {$endIf}
+end;
+{$endIf}
+
+{%endRegion}
+
+{$ifDef in0k_lazIdeSRC_SourceEditor_onActivate__inFocusONLY}
 {%region --- Active Window SourceEditor --------------------------- /fold}
 
 { ТЕКУЩЕЕ окно "Редактирования Исходного Кода"
@@ -232,7 +309,7 @@ begin
            _WSE_reStore_onDeactivate_(_ide_object_WSE_);
             {$ifDEF _debugLOG_}
             DEBUG('ERROR','_WSE_set_ inline var _ide_object_WSE_<>NIL');
-            ShowMessage('_WSE_set_ inline var _ide_object_WSE_<>NIL'+_c_DBG_PleaseReport_);
+            //ShowMessage('_WSE_set_ inline var _ide_object_WSE_<>NIL'+_c_DBG_PleaseReport_);
             {$endIf}
         end;
        _WSE_rePlace_onDeactivate_(wnd);
@@ -241,94 +318,7 @@ begin
 end;
 
 {%endRegion}
-
-{%region --- IdeEVENT semEditorActivate --------------------------- /fold}
-
-// основное рабочее событие
-procedure tIn0k_lazIdeSRC_SourceEditor_onActivate._ideEvent_SUMMARY;
-var tmpSourceEditor:TSourceEditorInterface;
-begin
-    {*1> причины использования _ide_object_ESE_
-        механизм приходится использовать из-за того, что
-        при переключение "Вкладок Редактора Исходного Кода" вызов данного
-        события происходит аж 3(три) раза. Используем только ПЕРВЫЙ вход.
-        -----
-        еще это событие происходит КОГДА идет навигация (прыжки внутри файла)
-    }
-    if Assigned(SourceEditorManagerIntf) then begin //< запредельной толщины презерватив
-        tmpSourceEditor:=SourceEditorManagerIntf.ActiveEditor;
-        if Assigned(tmpSourceEditor) then begin //< чуть потоньше, но тоже толстоват
-            if (tmpSourceEditor<>_ide_object_ESE_) then begin
-               _ESE_set_(tmpSourceEditor);
-            end
-            else begin
-                {$ifDEF _debugLOG_}
-                DEBUG('SKIP','already processed');
-                {$endIf}
-            end;
-        end
-        else begin
-           _ESE_set_(nil);
-            {$ifDEF _debugLOG_}
-            DEBUG('ER','ActiveEditor is NULL');
-            {$endIf}
-        end;
-    end
-    else begin
-        {$ifDEF _debugLOG_}
-        DEBUG('ER','IDE not ready');
-        {$endIf}
-    end;
-end;
-
-//------------------------------------------------------------------------------
-
-procedure tIn0k_lazIdeSRC_SourceEditor_onActivate._ideEvent_semEditorActivate(Sender:TObject);
-begin
-    {$ifDEF _debugLOG_}
-    DEBUG('ideEVENT:semEditorActivate','--->>>'+' sender'+addr2txt(Sender));
-    {$endIf}
-
-    //< запускаемся только если окно редактирования в ФОКУСЕ
-    if Assigned(_ide_object_WSE_) then _ideEvent_SUMMARY
-    else begin
-        {$ifDEF _debugLOG_}
-        DEBUG('SKIP','ActiveSourceWindow is UNfocused');
-        {$endIf}
-    end;
-
-    {$ifDEF _debugLOG_}
-    DEBUG('ideEVENT:semEditorActivate','---<<<');
-    {$endIf}
-end;
-
-procedure tIn0k_lazIdeSRC_SourceEditor_onActivate._ideEvent_semWindowFocused(Sender:TObject);
-begin
-    {$ifDEF _debugLOG_}
-    DEBUG('ideEVENT:semWindowFocused','--->>>'+' sender'+addr2txt(Sender));
-    {$endIf}
-
-    if Assigned(Sender) and (Sender is TSourceEditorWindowInterface) then begin
-       _WSE_set_(TSourceEditorWindowInterface(Sender));
-        if Assigned(_ide_object_WSE_) then _ideEvent_SUMMARY
-        else begin
-            {$ifDEF _debugLOG_}
-            DEBUG('SKIP WITH ERROR','BIG ERROR: ower _ide_Window_SEW_ found');
-            {$endIf}
-        end;
-    end
-    else begin
-        {$ifDEF _debugLOG_}
-        DEBUG('SKIP','Sender undef');
-        {$endIf}
-    end;
-
-    {$ifDEF _debugLOG_}
-    DEBUG('ideEVENT:semWindowFocused','---<<<');
-    {$endIf}
-end;
-
-{%endRegion}
+{$endIf}
 
 //------------------------------------------------------------------------------
 
@@ -342,15 +332,19 @@ end;
 // настроить LazarusIDE для работы
 procedure tIn0k_lazIdeSRC_SourceEditor_onActivate.LazarusIDE_SetUP;
 begin
-    SourceEditorManagerIntf.RegisterChangeEvent(semWindowFocused,  @_ideEvent_semWindowFocused);
     SourceEditorManagerIntf.RegisterChangeEvent(semEditorActivate, @_ideEvent_semEditorActivate);
+    {$ifDef in0k_lazIdeSRC_SourceEditor_onActivate__inFocusONLY}
+    SourceEditorManagerIntf.RegisterChangeEvent(semWindowFocused,  @_ideEvent_semWindowFocused);
+    {$endIf}
 end;
 
 // очистить наши настройки из LazarusIDE
 procedure tIn0k_lazIdeSRC_SourceEditor_onActivate.LazarusIDE_Clean;
 begin
-    SourceEditorManagerIntf.UnRegisterChangeEvent(semWindowFocused,  @_ideEvent_semWindowFocused);
     SourceEditorManagerIntf.UnRegisterChangeEvent(semEditorActivate, @_ideEvent_semEditorActivate);
+    {$ifDef in0k_lazIdeSRC_SourceEditor_onActivate__inFocusONLY}
+    SourceEditorManagerIntf.UnRegisterChangeEvent(semWindowFocused,  @_ideEvent_semWindowFocused);
+    {$endIf}
 end;
 
 end.
